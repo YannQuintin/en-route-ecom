d54ea2e537156cee35130b2951535a2163a73915
Yann Quintin <quintin.yann@gmail.com>
2021-01-20 23:53:16 +0100

    all

---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+import useStyles from './styles';
+
+
+// const products = [
+// 	{ id: 1, name: 'Shoes', description: 'Running shows', price: '160', image: 'https://www.fiets.nl/app/uploads/Apex-_V187330-Medium-800x525.jpg' },
+// 	{ id: 2, name: 'bike', description: 'Apex bike', price: '2000', image: 'https://www.fiets.nl/app/uploads/Apex-_V187330-Medium-800x525.jpg' },
+// ];
+
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles(); 
+
+	return (
+		<main className={classes.content}>
+        <div className={classes.toolbar} />
+			<Grid container justify="center" spacing={4}>
+				{products.map((product) => (
+					<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+						<Product product={product} onAddToCart={onAddToCart}/>
+					</Grid>
+				))}
+			</Grid>
+		</main>
+	);
+};
+
+export default Products;
